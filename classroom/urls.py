9234c867f72df8384aa7b9e91e4f61c1a3d20d65
Atheena <atheena@atheena-Latitude-E6230>
2022-02-07 19:45:35 +0530

    view -class name corrected (" changed by mistake ")

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import ClassRoomListView
+from .views import ClassRoomCreateView, ClassRoomListView
 
 urlpatterns = [
-    path("", ClassRoomListView.as_view(), name="classroom_list"),
-    path("classroom/create", ClassRoomListView.as_view(), name="classroom_create"),
+    path("", ClassRoomListView.as_view(), name="book_list"),
+    path("classroom/create", ClassRoomCreateView.as_view(), name="classroom_create"),
 ]
