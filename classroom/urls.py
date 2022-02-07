056dd847cf5f8a331c556fb91ce95215c7bf09ff
Atheena <atheena@atheena-Latitude-E6230>
2022-02-07 19:21:51 +0530

    urls name changed

--- classroom/urls.py
+++ classroom/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ClassRoomListView
 
 urlpatterns = [
-    path("", ClassRoomListView.as_view(), name="book_list"),
-    path("classroom/create", ClassRoomListView.as_view(), name="classroom"),
+    path("", ClassRoomListView.as_view(), name="classroom_list"),
+    path("classroom/create", ClassRoomListView.as_view(), name="classroom_create"),
 ]
