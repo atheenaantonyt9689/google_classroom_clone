ce7baf0eee3cd9b63201e5e9cc893e229a254782
Atheena <atheena@atheena-Latitude-E6230>
2022-02-07 10:57:54 +0530

    Update admin.py and models.py

--- classroom/models.py
+++ classroom/models.py
@@ -1,16 +1,14 @@
-from django.db import models 
+from django.db import models
 
 # Create your models here.
-
-
 class Classrooom(models.Model):
-    
+
     classname = models.CharField(max_length=30)
     section = models.CharField(max_length=30)
     subject = models.CharField(max_length=30)
     room = models.CharField(max_length=30)
     teacher = models.CharField(max_length=50)
-    class_code =models.CharField(max_length=30,null=True, blank=True)
+    class_code = models.CharField(max_length=30, null=True, blank=True)
 
     def __str__(self):
-        return  self.classname
+        return self.classname
