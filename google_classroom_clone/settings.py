6c0bd79ef3af7242c00a1eb2f8c1f0cb5383cf2f
Atheena <atheena@atheena-Latitude-E6230>
2022-03-20 19:15:34 +0530

    Update settings.py

--- google_classroom_clone/settings.py
+++ google_classroom_clone/settings.py
@@ -113,6 +113,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = LOGIN_URL
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
