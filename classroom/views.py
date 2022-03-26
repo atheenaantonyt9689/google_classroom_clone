91f77701bf21d2851a5474c4cb3cb1b00640a148
Atheena <atheena@atheena-Latitude-E6230>
2022-03-26 14:34:46 +0530

    code formatted

--- classroom/views.py
+++ classroom/views.py
@@ -1,6 +1,12 @@
 from django.contrib import messages
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView,DeleteView,DetailView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView,
+)
 from classroom.forms import ClassroomCreateForm, ClassroomEditForm
 from classroom.models import Classrooom
 from django.urls import reverse, reverse_lazy
@@ -14,32 +20,31 @@ from django.contrib.auth.mixins import AccessMixin
 #             return self.handle_no_permission()
 #         return super().dispatch(request,*args,**kwargs)
 
-class TeacherRequiredMixin(AccessMixin):
 
-    def dispatch( self,request,*args,**kwargs):
+class TeacherRequiredMixin(AccessMixin):
+    def dispatch(self, request, *args, **kwargs):
         if not request.user.is_teacher:
             return self.handle_no_permission()
-        return super().dispatch(request,*args,**kwargs)
-
-
+        return super().dispatch(request, *args, **kwargs)
 
 
 # Create your views here.
 
-class ClassRoomListView(LoginRequiredMixin,ListView):
+
+class ClassRoomListView(LoginRequiredMixin, ListView):
 
     model = Classrooom
     template_name = "classroom/classroom_list.html"
-    login_url = 'login'
+    login_url = "login"
 
 
-class ClassRoomCreateView(LoginRequiredMixin,TeacherRequiredMixin,CreateView):
+class ClassRoomCreateView(LoginRequiredMixin, TeacherRequiredMixin, CreateView):
 
     template_name = "classroom/new_classroom_create.html"
     model = "Classroom"
     form_class = ClassroomCreateForm
     success_url = reverse_lazy("classroom_list")
-    login_url = 'login'
+    login_url = "login"
 
     def form_valid(self, form):
 
@@ -51,13 +56,13 @@ class ClassRoomCreateView(LoginRequiredMixin,TeacherRequiredMixin,CreateView):
         return super().form_invalid(form)
 
 
-class ClassRoomEditView(LoginRequiredMixin,TeacherRequiredMixin,UpdateView):
+class ClassRoomEditView(LoginRequiredMixin, TeacherRequiredMixin, UpdateView):
 
     template_name = "classroom/classroom_edit.html"
     model = Classrooom
     form_class = ClassroomEditForm
     success_url = reverse_lazy("classroom_list")
-    login_url = 'login'
+    login_url = "login"
 
     def form_valid(self, form):
 
@@ -69,20 +74,16 @@ class ClassRoomEditView(LoginRequiredMixin,TeacherRequiredMixin,UpdateView):
         return super().form_invalid(form)
 
 
-class ClassroomDeleteView(LoginRequiredMixin,TeacherRequiredMixin,DeleteView):
+class ClassroomDeleteView(LoginRequiredMixin, TeacherRequiredMixin, DeleteView):
 
     model = Classrooom
-    template_name = 'classroom/classroom_delete.html'
-    success_url = reverse_lazy('classroom_list')
-    login_url = 'login'
+    template_name = "classroom/classroom_delete.html"
+    success_url = reverse_lazy("classroom_list")
+    login_url = "login"
 
 
-class ClassrooomDetailview(LoginRequiredMixin,DetailView):
+class ClassrooomDetailview(LoginRequiredMixin, DetailView):
 
     model = Classrooom
-    template_name = 'classroom/teacher_dashboard.html'
-    login_url = 'login'
-
-
-
-
+    template_name = "classroom/teacher_dashboard.html"
+    login_url = "login"
