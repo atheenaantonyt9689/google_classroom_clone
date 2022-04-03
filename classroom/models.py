f9403976b1560c220af7ac5c215b5ffd68163601
Atheena <atheenaantonyt@gmail.com>
2022-04-03 22:41:51 +0530

    assignment class model created

--- classroom/models.py
+++ classroom/models.py
@@ -1,4 +1,6 @@
+from tkinter import CASCADE
 from django.db import models
+from accounts.models import Student
 
 # Create your models here.
 class Classrooom(models.Model):
@@ -13,3 +15,25 @@ class Classrooom(models.Model):
 
     def __str__(self):
         return self.classname
+
+
+class Assignment(models.Model):
+
+    classroom = models.ForeignKey(Classrooom,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    instructions = models.CharField(max_length=250)
+    # files = models.ManyToManyField(FeedFiles)
+    student = models.ManyToManyField(Student)
+    points = models.IntegerField()
+    due_date =models.DateField()
+    topic = models.CharField(max_length=100)
+    date_time = models.DateTimeField(auto_now=True)
+
+class FeedFiles(models.Model): 
+
+    title = models.CharField(max_length=100)
+    files = models.FileField()
+    assignment = models.ForeignKey(Assignment,on_delete=models.CASCADE)
+
+
+
