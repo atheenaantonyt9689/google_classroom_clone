d0b16c2b65fd7827aee8419f4777d6ee8205a538
Atheena <atheena@atheena-Latitude-E6230>
2022-04-13 14:36:23 +0530

    some fields added to assignmnet and feedfile class

--- classroom/models.py
+++ classroom/models.py
@@ -11,29 +11,29 @@ class Classrooom(models.Model):
     room = models.CharField(max_length=30)
     teacher = models.CharField(max_length=50)
     class_code = models.CharField(max_length=30, null=True, blank=True)
-    image = models.ImageField(upload_to='classroom_gallery/',null=True, blank=True)
+    image = models.ImageField(upload_to="classroom_gallery/", null=True, blank=True)
 
     def __str__(self):
         return self.classname
 
 
 class Assignment(models.Model):
-
-    classroom = models.ForeignKey(Classrooom,on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    instructions = models.CharField(max_length=250)
-    # files = models.ManyToManyField(FeedFiles)
-    student = models.ManyToManyField(Student)
-    points = models.IntegerField()
-    due_date =models.DateField()
+    classroom = models.ForeignKey(Classrooom, on_delete=models.CASCADE)
     topic = models.CharField(max_length=100)
-    date_time = models.DateTimeField(auto_now=True)
+    instructions = models.TextField(max_length=250)
+    student = models.ManyToManyField(Student)
+    points = models.IntegerField(default=10)
+    due_date = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True)
+    # updated_at = models.DateTimeField(auto_now=True)
 
-class FeedFiles(models.Model): 
-
-    title = models.CharField(max_length=100)
-    files = models.FileField()
-    assignment = models.ForeignKey(Assignment,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.topic
 
 
-
+class FeedFile(models.Model):
+    title = models.CharField(max_length=100, null=True, blank=True)
+    files = models.FileField(upload_to="assignment_files/", null=True, blank=True)
+    assignment = models.ForeignKey(
+        Assignment, on_delete=models.SET_NULL, null=True, default=None
+    )
