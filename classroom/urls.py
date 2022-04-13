2171ec6e06e3521ae45755e03bea636b2c255cc1
Atheena <atheena@atheena-Latitude-E6230>
2022-04-13 16:37:03 +0530

    Update urls.py

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import (
-    AssignmentCreateView,
+    AssignmentCreateView,    
     ClassRoomCreateView,
     ClassRoomListView,
     ClassRoomEditView,
@@ -13,20 +13,8 @@ urlpatterns = [
     path("", ClassRoomListView.as_view(), name="classroom_list"),
     path("classroom/create", ClassRoomCreateView.as_view(), name="classroom_create"),
     path("classroom/<int:pk>/edit", ClassRoomEditView.as_view(), name="classroom_edit"),
-    path(
-        "classroom/<int:pk>/delete",
-        ClassroomDeleteView.as_view(),
-        name="classroom_delete",
-    ),
-    path(
-        "classroom/<int:pk>/details",
-        ClassrooomDetailview.as_view(),
-        name="classroom_detailview",
-    ),
+    path("classroom/<int:pk>/delete",ClassroomDeleteView.as_view(),name="classroom_delete"),
+    path("classroom/<int:pk>/details",ClassrooomDetailview.as_view(),name="classroom_detailview"),
     # assignment_part
-    path(
-        "assignment/create",
-        AssignmentCreateView.as_view(),
-        name="classroom_assignment_create",
-    ),
+    path("assignment/create",AssignmentCreateView.as_view(),name="classroom_assignment_create"),
 ]
